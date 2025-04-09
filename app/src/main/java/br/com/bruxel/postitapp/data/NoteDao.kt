921458b91fe8968d5e181bc49559bd57b9d9c776
bruxel <alexbruxel@gmail.com>
2025-04-09 19:29:19 -0300

    Fix ajustes NoteDao, NoteRepository

--- app/src/main/java/br/com/bruxel/postitapp/data/NoteDao.kt
+++ app/src/main/java/br/com/bruxel/postitapp/data/NoteDao.kt
@@ -4,14 +4,12 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import br.com.bruxel.postitapp.model.Note
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface NoteDao {
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insert(note: Note)
-
     @Query("SELECT * FROM Note WHERE isArchived = 0 AND isDeleted = 0 ORDER BY timestamp DESC")
     fun getActiveNotes(): Flow<List<Note>>
 
@@ -26,4 +24,19 @@ interface NoteDao {
 
     @Query("UPDATE Note SET isDeleted = :value WHERE id = :noteId")
     suspend fun setDeleted(noteId: Int, value: Boolean)
+
+    @Query("SELECT * FROM Note WHERE id = :noteId")
+    fun getNoteById(noteId: Int): Flow<Note?>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun saveNote(note: Note)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(note: Note)
+
+    @Update
+    suspend fun update(note: Note)
+
+    @Query("SELECT * FROM Note WHERE 1=1 ORDER BY timestamp DESC")
+    fun getAll(): Flow<List<Note>>
 }
--- app/src/main/java/br/com/bruxel/postitapp/repository/NoteRepository.kt
+++ app/src/main/java/br/com/bruxel/postitapp/repository/NoteRepository.kt
@@ -2,6 +2,7 @@ package br.com.bruxel.postitapp.repository
 
 import br.com.bruxel.postitapp.data.NoteDao
 import br.com.bruxel.postitapp.model.Note
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class NoteRepository @Inject constructor(private val dao: NoteDao) {
@@ -9,11 +10,16 @@ class NoteRepository @Inject constructor(private val dao: NoteDao) {
     val archivedNotes = dao.getArchivedNotes()
     val deletedNotes = dao.getDeletedNotes()
 
-    suspend fun insert(note: Note) = dao.insert(note)
-
     suspend fun toggleArchive(note: Note) = dao.setArchived(note.id, !note.isArchived)
 
     suspend fun delete(note: Note) = dao.setDeleted(note.id, true)
-
     suspend fun restore(note: Note) = dao.setDeleted(note.id, false)
+
+    fun getNoteById(id: Int): Flow<Note?> = dao.getNoteById(id)
+
+    suspend fun save(note: Note) = dao.saveNote(note)
+    suspend fun insert(note: Note) = dao.insert(note)
+    suspend fun update(note: Note) = dao.update(note)
+
+    fun getAllNotes(): Flow<List<Note>> = dao.getAll()
 }
