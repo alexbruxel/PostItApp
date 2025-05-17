db0fc633b4981ce3b5bd730fd751364c4949a771
bruxel <alexbruxel@gmail.com>
2025-05-17 04:12:21 -0300

    Alterado versão da 8.2.2 para 8.10.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
 //    alias(libs.plugins.android.application) apply false
 //    alias(libs.plugins.kotlin.android) apply false
 
-    id("com.android.application") version "8.2.2" apply false
+    id("com.android.application") version "8.10.0" apply false
     kotlin("android") version "1.9.20" apply false
     id("com.google.devtools.ksp") version "1.9.21-1.0.16" apply false
     id("com.google.dagger.hilt.android") version "2.50" apply false
